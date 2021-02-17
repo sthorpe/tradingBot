d29ec5210599dfeb466f8de0c3cdcc139897318d
Scott Thorpe <sthorpe@gmail.com>
2021-02-17 14:26:11 -0700

    Building out the coinbase class

--- DataStreams/Coinbase.py
+++ DataStreams/Coinbase.py
@@ -14,3 +14,17 @@ class Coinbase():
     def client(self):
         auth_client = cbpro.AuthenticatedClient(self.api_key, self.secret_key, self.passphrase, api_url="https://api-public.sandbox.pro.coinbase.com")
         return auth_client
+
+    def get_accounts(self):
+        return self.client().get_accounts()
+
+    def get_account(self, account_number):
+        return self.client().get_account(account_number)
+
+    def buy(self):
+        # Buy 0.01 BTC @ 100 USD
+        self.client().buy(price='100.00', size='0.01', order_type='limit', product_id='BTC-USD')
+        
+    def sell(self):
+        # Sell 0.01 BTC @ 200 USD
+        self.client().sell(price='200.00', size='0.01', order_type='limit', product_id='BTC-USD')
--- main.py
+++ main.py
@@ -3,17 +3,16 @@ import requests
 import pprint
 from DataStreams.Coinbase import Coinbase
 
+# Sandbox keys
+COINBASE_PRO_KEY = 'c606224329f6a3a18d25a22436cea960'
+COINBASE_PRO_SECRET = 'p6Ne820xWPrGekpAd1cdtyF6MTmgT1V55DIotyCWENuSQj6IqseFdfBjyRoFdQ9FqrNgbSovaF3fw2SsoSjgzQ=='
+COINBASE_PRO_PASSPHRASE = 'lb3t6qfupvo'
+
 # This will be where we combine all our functions and run them
 def main():
-    COINBASE_PRO_KEY = 'c606224329f6a3a18d25a22436cea960'
-    COINBASE_PRO_SECRET = 'p6Ne820xWPrGekpAd1cdtyF6MTmgT1V55DIotyCWENuSQj6IqseFdfBjyRoFdQ9FqrNgbSovaF3fw2SsoSjgzQ=='
-    COINBASE_PRO_PASSPHRASE = 'lb3t6qfupvo'
-    # api_url = 'https://api.pro.coinbase.com/'
-    api_url = 'https://api-public.sandbox.pro.coinbase.com'
-    auth = Coinbase(COINBASE_PRO_KEY, COINBASE_PRO_SECRET, COINBASE_PRO_PASSPHRASE)
-    client = auth.client()
+    coinbaseAPI = Coinbase(COINBASE_PRO_KEY, COINBASE_PRO_SECRET, COINBASE_PRO_PASSPHRASE)
     pp = pprint.PrettyPrinter(indent=4)
-    pp.pprint(client.get_accounts())
+    pp.pprint(coinbaseAPI.get_accounts())
 
 if __name__ == "__main__":
     main()
